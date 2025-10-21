a88ea72ab428fcdf8c29a50dcbf595493016b0f9
Moralex19 <dominguezmoralesfreddy@gmail.com>
2025-10-21 08:53:41 -0600

    Initial Commit

---
+++ ecosmoothie/ecosmoothieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecosmoothieApp.swift
+//  ecosmoothie
+//
+//  Created by Freddy Morales on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ecosmoothieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
